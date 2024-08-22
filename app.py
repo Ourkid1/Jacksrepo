9356bdc3bcf9925519df6d7c62b72382f668d10e
Ourkid1 <147047055+Ourkid1@users.noreply.github.com>
2024-08-22 23:25:41 +0100

    Update app.py
    added mysql code update to reflect into GCP environment. first, commit.

--- app.py
+++ app.py
@@ -4,52 +4,42 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
-#SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
-#    username="bannisterj",
-#    password="",
-#    hostname="bannisterj.mysql.pythonanywhere-services.com",
-#    databasename="bannisterj$comments",
-#)
-#app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
-#app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
-#app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-#
-#db = SQLAlchemy(app)
+# Replace these values with your actual database configuration
+username = "root"
+password = "password1"
+hostname = "172.17.192.3"  # private IP
+databasename = "comments"
 
-#class Comment(db.Model):
+SQLALCHEMY_DATABASE_URI = f"mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}"
 
-#    __tablename__ = "comments"
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-#    id = db.Column(db.Integer, primary_key=True)
-#    content = db.Column(db.String(4096))
+db = SQLAlchemy(app)
 
-#@app.route("/contact", methods=["GET", "POST"])
-#def contact():
-#    if request.method == "GET":
-#        return render_template('contact.html')
- #       return render_template("contact.html", comments=Comment.query.all())
+class Comment(db.Model):
+    __tablename__ = "comments"
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(4096))
 
- #   comment = Comment(content=request.form["contents"])
- #   db.session.add(comment)
- #   db.session.commit()
- #   return redirect(url_for('contact'))
+@app.route("/contact", methods=["GET", "POST"])
+def contact():
+    if request.method == "GET":
+        return render_template("contact.html", comments=Comment.query.all())
 
+    comment = Comment(content=request.form["contents"])
+    db.session.add(comment)
+    db.session.commit()
+    return redirect(url_for('contact'))
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
-@app.route('/about')  #  '/about'
+@app.route('/about')
 def about():
     return render_template('about.html')
-    
-@app.route("/contact", methods=["GET", "POST"])
-def contact():
-    if request.method == "GET":
-        return render_template('contact.html')
-
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
