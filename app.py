855110b47cc808dfe32f1a9ac75d07cb0cc0ce99
Ourkid1 <147047055+Ourkid1@users.noreply.github.com>
2024-08-23 19:44:22 +0100

    Update app.py
    applied a fix to comments

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def contact():
     if request.method == "GET":
         return render_template("contact.html", comments=Comment.query.all())
 
-    comment = Comment(content=request.form["contents"])
+    comment = Comment(content=request.form["comment"])
     db.session.add(comment)
     db.session.commit()
     return redirect(url_for('contact'))
