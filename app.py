c9b6297c9d6240e8f8df6b72a167b8ddcaaac7a6
Ourkid1 <147047055+Ourkid1@users.noreply.github.com>
2024-08-27 14:34:28 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,13 @@
+# app.py
+
 from flask import Flask, redirect, render_template, request, url_for
 from flask_sqlalchemy import SQLAlchemy
+from constants import DATABASE_USERNAME, DATABASE_PASSWORD, DATABASE_HOSTNAME, DATABASE_NAME
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
-SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
-    username="root",
-    password="password1",
-    hostname="34.41.207.237",
-    databasename="comments",
-)
-app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_DATABASE_URI"] = f"mysql+mysqlconnector://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_HOSTNAME}/{DATABASE_NAME}"
 app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
@@ -26,9 +23,14 @@ class Comment(db.Model):
 @app.route("/contact", methods=["GET", "POST"])
 def contact():
     if request.method == "GET":
-        return render_template("contact.html", comments=Comment.query.all())
+        try:
+            comments = Comment.query.all()
+            return render_template("contact.html", comments=comments)
+        except Exception as e:
+            print(f"Error fetching comments: {e}")
+            return "An error occurred while fetching comments."
 
-    comment = Comment(content=request.form["contents"])
+    comment = Comment(content=request.form["comment"])
     db.session.add(comment)
     db.session.commit()
     return redirect(url_for('contact'))
@@ -45,5 +47,4 @@ def about():
 
 # Run the application
 if __name__ == '__main__':
-    # Ensure the app is accessible externally and on port 5000
     app.run(host='0.0.0.0', port=5000)
