be45f54b1996660191a41c80dc97cb05e65eb754
Ourkid1 <147047055+Ourkid1@users.noreply.github.com>
2024-09-08 16:03:13 +0100

    Update constants.py

--- constants.py
+++ constants.py
@@ -4,5 +4,5 @@
 DATABASE_USERNAME = "root"
 DATABASE_PASSWORD = "password1"
 DATABASE_HOSTNAME = "172.17.192.3"
-DATABASE_NAME = "comments"
+DATABASE_NAME = "mysqldb"
 port=3306  # MySQL default port
